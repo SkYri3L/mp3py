b4265bf5816ee0d941a552c9ce7433f65a7e70f6
SkYri3L <Skyrimplaynet@gmail.com>
2022-08-13 18:05:19 +0100

    First Commit

--- Main.py
+++ Main.py
@@ -1,17 +1,114 @@
-import pygame
+import pygame, os
+from pygame import mixer
+
+mixer.init()
+
 Open = 1
-pygame.mixer.init()
-pygame.mixer.music.load('Songs/Song.mp3')
+
+def clearconsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):
+        command = 'cls'
+    os.system(command)
+
+def song_dir():
+    global track
+    global Dir
+    Dir = input("Please Enter Songs Directory: ")
+    track = os.listdir(Dir)
+
+def song_reset():
+    global playing
+    global paused
+    global status
+    status = "Music Stopped"
+    paused = False
+    playing = False
+
+
+def Select():
+    global Song_Select
+    global track
+    mixer.music.unload()
+    num = 0
+    for tracks in track:
+        print(num,": "+tracks)
+        num = num + 1
+    Song_Select = int(input("Enter Track Number: "))
+    print(track[Song_Select])
+    song = (Dir+"/"+track[Song_Select])
+    print(song)
+    mixer.music.load(song)
+
+def playsong():
+    global status
+    if playing == False:
+        mixer.music.play()
+        status = "Music Playing"
+    elif playing == True:
+        pass
+    else:
+        pass
+
+
+def stopsong():
+    global status
+    mixer.music.stop()
+    status = "Music Stopped"
+def pausesong():
+    global status
+    if paused == True:
+        pass
+    elif paused == False:
+        mixer.music.pause()
+        status = "Music Paused"
+    else:
+        pass
+
+def unpausesong():
+    global status
+    if paused == True:
+        mixer.music.unpause()
+        status = "Music Playing"
+    elif paused == False:
+        pass
+    else:
+        pass
+
+song_dir()
+Select()
+song_reset()
 
 
 while Open == 1:
-    print("Current Song: ")
-    check = input("Play/Stop/Exit :")
-    if check == "P" or check == "Play" or check == "play" or check == "p":
-        pygame.mixer.music.play()
-    elif check == "S" or check == "stop" or check == "Stop" or check == "s":
-        pygame.mixer.music.stop()
-    elif check == "Exit" or check == "exit" or check == "E" or check == "e":
+    clearconsole()
+
+
+
+    print("Current Song: ", track[Song_Select])
+    print("Current Status: ", status)
+
+
+
+    check = input("1: Play\n2: Stop\n3: Pause\n4:Unpause\n5: Song Select\n6: Change Directoy\n7:Exit\nEnter Number:")
+
+
+    if check == "1" or check == "play" or check == "Play":
+        playsong()
+    elif check == "2" or check == "stop" or check == "Stop":
+        stopsong()
+    elif check == "3" or check == "Pause" or check == "pause":
+        pausesong()
+    elif check == "4" or check == "Unpause" or check == "unpause":
+        unpausesong()
+    elif check == "5" or check == "Song" or check == "song":
+        mixer.music.stop()
+        song_reset()
+        Select()
+    elif check == "6" or check == "Change" or check == "directory" or check == "Change Directory" or check == "Directory" or check == "change" or check == "dir" or check == "Dir":
+        mixer.music.stop()
+        song_reset()
+        song_dir()
+    elif check == "7" or check == "exit" or check == "Exit":
         quit()
-    else:
-        print("Wrong Input")
+
